d77c3d89990f9546e3d138e43a4243a8d85e15c3
Liia Hänninen <liiahanninen@Liia-MacBook-Pro.local>
2021-03-29 19:42:49 +0300

    tehtava 6.2

--- src/main/java/fi/hh/swd20/bookstore/BookstoreApplication.java
+++ src/main/java/fi/hh/swd20/bookstore/BookstoreApplication.java
@@ -11,6 +11,8 @@ import fi.hh.swd20.bookstore.domain.Book;
 import fi.hh.swd20.bookstore.domain.BookRepository;
 import fi.hh.swd20.bookstore.domain.Category;
 import fi.hh.swd20.bookstore.domain.CategoryRepository;
+import fi.hh.swd20.bookstore.domain.User;
+import fi.hh.swd20.bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -23,7 +25,7 @@ public class BookstoreApplication {
 	
 	
 	@Bean
-	public CommandLineRunner bookstoreDemo (CategoryRepository categoryrepository, BookRepository bookrepository) {
+	public CommandLineRunner bookstoreDemo (CategoryRepository categoryrepository, BookRepository bookrepository, UserRepository userrepository) {
 		return (args) -> {
 			// luodaan testidataan kategoriat
 			log.info("save a couple of categories");
@@ -43,6 +45,15 @@ public class BookstoreApplication {
 			Book book3 = new Book("Harry Potter and the prisoner of Azkaban", "J.K. Rowling", "456789", 11.90, category1);
 			bookrepository.save(book3);
 			
+			// luodaan testikäyttäjiä
+			log.info("save couple of users");
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "user@user.fi", "USER");
+			userrepository.save(user1);
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "admin@admin.fi", "ADMIN");
+			userrepository.save(user2);
+			User user3 = new User("liia", "$2a$10$eLgmttXgTYdiq3ieqTXR8OBKWgdF/AERV6DoG84GjmrD/LQ30CKRy", "liia@liia.fi", "ADMIN");
+			userrepository.save(user3);
+			
 			log.info("Fetch all the books");
 			for (Book book : bookrepository.findAll()) {
 			   log.info(book.toString());
@@ -51,6 +62,11 @@ public class BookstoreApplication {
 			log.info("Fetch all the categories");
 			for (Category category : categoryrepository.findAll()) {
 			   log.info(category.toString());
+			}
+			
+			log.info("Fetch all the users");
+			for (User user : userrepository.findAll()) {
+			   log.info(user.toString());
 			}
 			
 		};

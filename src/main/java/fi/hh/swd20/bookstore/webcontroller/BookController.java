49b755be60bebaa354ee0f25280a6254423ef8de
Liia Hänninen <liiahanninen@Liia-MacBook-Pro.local>
2021-03-29 18:50:03 +0300

    korjaukset tehtävään 6.1

--- src/main/java/fi/hh/swd20/bookstore/webcontroller/BookController.java
+++ src/main/java/fi/hh/swd20/bookstore/webcontroller/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -73,6 +74,7 @@ public class BookController {
 	
 	// poistetaan kirja idn perusteella
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	@PreAuthorize("hasRole('ADMIN')")
 	public String delete (@PathVariable(value= "id") long id) {
 		bookrepository.deleteById(id);
 		return "redirect:/booklist";
